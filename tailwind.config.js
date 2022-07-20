8790acda528aa1b0046366497b69407acb30c36a
Calil Sousa Mattos <contatocsmpm@gmail.com>
2022-07-20 11:52:51 -0300

    commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '320px',
+
+      'sm2': '375px',
+
+      'sm3': '425px',
+
+      'md': '768px',
+      
+      'ip': '810px',
+
+      'lg': '1024px',
+      
+      'xl': '1440px',
+    },
+    backgroundImage: {
+      'contact': "url('assets/pexels-contact.jpg')",
+    },
+    extend: {
+      colors: {
+        'footer': '#1E2019',
+        'tab': 'rgb(0,255,128)'
+      },
+    },
+  },
+  plugins: [],
+}
